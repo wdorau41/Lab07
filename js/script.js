f5098ea882cfb888499ef8149a990104dd28db8b
wdorau41 <54552856+wdorau41@users.noreply.github.com>
2019-11-20 06:34:35 -0600

    attempt 3

--- js/script.js
+++ js/script.js
@@ -2,10 +2,17 @@ function init(){
 
 
   document.getElementById("entrybutton").addEventListener("click", myfunction());
+  document.getElementById("entrybutton").addEventListener("click", myfunction1());
 }
 
 function myfunction(){
-  alert("Walter Dorau:", getElementById("textentry"));
+  var x = document.getElementById("entryinput");
+  alert("Walter Dorau:", x);
+}
+function myfunction1(){
+  var y = document.getElementById("entryinput");
+  var z = document.getElementById("textoutput");
+  z.style.display(y);
 }
 
 
